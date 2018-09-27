327a060ad8cfe8840ef13d59771b4d8a065ddb3a
Дмитрий <mac@MacBook-Pro-Dmitry.local>
2018-09-27 20:53:28 +0300

    logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,10 @@ import { HttpClient, HttpResponse, HttpParams, HttpHeaders, HttpErrorResponse } 
 export class AuthService {
   private access_token: string;
   private token_type: string;
-  constructor(private client: HttpClient, private config: ConfigService) { }
+  constructor(private client: HttpClient, private config: ConfigService) {
+    this.access_token = this._getCookie('access_token');
+    this.token_type = this._getCookie('token_type');
+  }
   /**
    * login
    */
@@ -21,12 +24,20 @@ export class AuthService {
     console.log(p);
     this.client
       .post<any>(this.config.host + "Token", p)
-      .subscribe(c => { this.saveSession(c.access_token, c.token_type); success(c); }, c => error(c));
+      .subscribe(c => { this.saveSession(c.access_token, c.token_type, c.expires_in); success(c); }, c => error(c));
   }
 
-  public saveSession(at: string, tt: string) {
+  public logout() {
+    document.cookie = "access_token=; path=/; expires=" + new Date(0).toUTCString();
+    document.cookie = "token_type=; path=/; expires=" + new Date(0).toUTCString();
+  }
+
+  public saveSession(at: string, tt: string, ei: string) {
+    console.log(ei);
     this.access_token = at;
     this.token_type = tt;
+    document.cookie = 'access_token=' + this.access_token + '; path=/; expires=' + ei;
+    document.cookie = 'token_type=' + this.token_type + '; path=/; expires=' + ei;
   }
 
   public get<T>(url: string, success?: (v: T) => void, error?: (v: T) => void) {
@@ -35,8 +46,8 @@ export class AuthService {
   }
 
   public post<T>(url: string, body?: any, success?: (v: T) => void, error?: (v: T) => void) {
-    this.client.post<T>(this.config.host + url, body, { headers: this._addAuth() })
-      .subscribe(c => success(c), c => error(c));
+    this.client.post<any>(this.config.host + url, body, { headers: this._addAuth() })
+      .subscribe(c => { success(c); }, c => error(c));
   }
 
   private _addAuth(h?: HttpHeaders): HttpHeaders {
@@ -45,5 +56,12 @@ export class AuthService {
     }
 
     return h.set('Authorization', (this.token_type ? this.token_type : '') + ' ' + (this.access_token ? this.access_token : ''));
+  }
+
+  private _getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
   }
 }
