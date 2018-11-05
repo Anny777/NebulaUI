964f5dd797026da49286f84b0936060f00c6fc55
Anny777 <bystrova_777@icloud.com>
2018-11-05 22:31:25 +0300

    уведомления

--- src/app/hall1/hall1.component.ts
+++ src/app/hall1/hall1.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastsManager } from 'ng6-toastr';
+import { ListDishService } from '../services/dish-order.service';
 
 @Component({
   selector: 'app-hall1',
@@ -9,13 +10,16 @@ import { ToastsManager } from 'ng6-toastr';
 })
 export class Hall1Component implements OnInit {
 
-  constructor(private router: Router, public toastr: ToastsManager, vcr: ViewContainerRef) {
+  constructor(private router: Router, private listDish: ListDishService, public toastr: ToastsManager, vcr: ViewContainerRef) {
     this.toastr.setRootViewContainerRef(vcr);
   }
 
   ngOnInit() {
+    this.listDish.OnDishReady.subscribe(r =>  this.response(r));
   }
-  toast(){
-    this.toastr.success('You are awesome!', 'Success!');
+
+  public response(dish: any){
+    console.log(dish);
+    this.toastr.success(dish.Name + "Стол № " + dish.Table, 'Готово!');
   }
 }
