29337cbecfc9890719ebb97966566474313630bf
Дмитрий <mac@MacBook-Pro-Dmitry.local>
2018-11-06 23:00:13 +0300

    example of using ngrx/store for @anny_777

--- src/app/components/forgot/forgot.component.ts
+++ src/app/components/forgot/forgot.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IOrder } from '../../models/order';
-import { AppState } from 'src/app.state';
+import { IAppState } from '../../store/app.state';
 import { Store } from '@ngrx/store';
+import * as OrderActions from '../../store/actions/orderActions'
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-forgot',
@@ -11,11 +13,12 @@ import { Store } from '@ngrx/store';
 })
 export class ForgotComponent implements OnInit {
 orders$: Observable<IOrder[]>
-  constructor(private store: Store<AppState>) {
-    this.orders$ = store.select("orders");
+  constructor(private store: Store<IAppState>) {
   }
 
   ngOnInit() {
+    this.orders$ = this.store.select(c=>c.orders.orders).pipe(tap(orders => console.log(orders)));
+    this.store.dispatch(new OrderActions.LoadOrders())
   }
 
 }
